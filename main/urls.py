7d81ceaaaf92e555f690a6e5c640ccc6cb22daba
Fabio Gelbcke <fabio.gelbcke@gmail.com>
2016-10-03 00:28:56 +0200

    added google webmaster verification file

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 urlpatterns = patterns('',
                        url(r'^index$', TemplateView.as_view(template_name='index.html'), name='index'),
                        url(r'^sobre$', TemplateView.as_view(template_name='about.html'), name='about'),
+                       url(r'^google449e81b63167a1b6.html', TemplateView.as_view(template_name='google449e81b63167a1b6.html'), name='google_webmaster'),
 )
