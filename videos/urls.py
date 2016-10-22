6654527a4eb433d6ee3021ed2521b6b724ca98f9
Fabio Gelbcke <fabio.gelbcke@gmail.com>
2016-10-22 19:43:28 +0200

    caching a buncha pages

--- videos/urls.py
+++ videos/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import patterns, url, include
 from django.views.generic import TemplateView
 import views
 from django.contrib.admin.views.decorators import staff_member_required
+from django.views.decorators.cache import cache_page
+
 urlpatterns = patterns('',
                        url(r'^video/(?P<slug>\d+)$',
-                           views.VideoView.as_view()
-                           , name='video_page'),
+                           cache_page(5*60*60)(views.VideoView.as_view()),
+                           name='video_page'),
 )
