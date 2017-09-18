5b552e835d734e72949860fe9569ddf56182d9af
Fabio Gelbcke <fabio.gelbcke@gmail.com>
2017-09-18 22:10:47 +0200

    removed cache from front page, added new buttons to course item pages that go to course info/progress

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from .views import IndexView, AboutView
 from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-               url(r'^$', cache_page(5*60*60)(IndexView.as_view()), name='index'),
-               url(r'^sobre$', cache_page(5*60*60)(AboutView.as_view()), name='about'),
+               url(r'^$', IndexView.as_view(), name='index'),
+               url(r'^sobre$', AboutView.as_view(), name='about'),
                url(r'^google449e81b63167a1b6.html', TemplateView.as_view(template_name='google449e81b63167a1b6.html'), name='google_webmaster'),
 ]
