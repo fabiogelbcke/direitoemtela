2d5cd245d41184d03548c2a658bd7ba3252c51cc
Fabio Gelbcke <fabio.gelbcke@gmail.com>
2017-11-10 16:28:02 +0100

    fixed little problem in main/urls

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^login$', LoginView.as_view(), name='index'),
+    url(r'^login$', LoginView.as_view(), name='login'),
     url(r'^sobre$', AboutView.as_view(), name='about'),
     url(r'^google449e81b63167a1b6.html', TemplateView.as_view(template_name='google449e81b63167a1b6.html'), name='google_webmaster'),
 ]
